6efb720fe65a04110e844906249964ccb6a278b9
ambulcao <ambulcao@gmail.com>
2022-03-02 21:50:15 +0000

    Add audio project

--- main.js
+++ main.js
@@ -5,19 +5,55 @@ console.log(keys)
 
 // play notes
 function playNote(event){
-  // keyCode
+  
+  let audioKeyCode = getKeyCode(event);
   
   // typed or pressed key
-
+  const key = document.querySelector(`.key[data-key="${audioKeyCode}"]`)
+  
   // if key exists
-  // console.log(event)
+  const cantFoundAnyKey = !key
+
+  if(cantFoundAnyKey) {
+    return;
+  }
 
   // play audio
+  addPlayingClass(key)
+  playAudio(audioKeyCode)
+}
+
+function addPlayingClass(key) {
+  key.classList.add('playing')
+}
+
+function getKeyCode(event) {
+  let keyCode;
+
+  const isKeyboard = event.type === "keydown" // true or false
+  if(isKeyboard) {
+    keyCode = event.keyCode
+  } else {
+    keyCode = event.target.dataset.key
+  }
+  return keyCode
+}
+
+function playAudio(audioKeyCode){
+  const audio = document.querySelector(`audio[data-key="${audioKeyCode}"]`)
+  audio.currentTime = 0;
+  audio.play()
+}
+
+function removePlayingClass(event){
+  event.target.classList.remove("playing")
 }
 
 // click with mouse
 keys.forEach(function(key){
-  console.log(key.dataset.note)
+  key.addEventListener("click", playNote)
+  key.addEventListener("transitionend", removePlayingClass)
+  //console.log(key.dataset.note)
 })
 
 // keyboard type
